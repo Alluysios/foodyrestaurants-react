7bfd88ee47eaa6f002f1be9d7889c4f8f3433fd9
Alluysios <arribaallu@gmail.com>
2020-09-30 19:26:37 -0600

    Updates

---
+++ precache-manifest.8dbb6efc96e2567cd5ee2355d6b783b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab818b492330dae4aff2d6541f9b00e",
+    "url": "/foodyrestaurants-react/index.html"
+  },
+  {
+    "revision": "a58b9d9bc24e408b3b0e",
+    "url": "/foodyrestaurants-react/static/css/main.f6b05f27.chunk.css"
+  },
+  {
+    "revision": "6421b4bacece1e0b78a3",
+    "url": "/foodyrestaurants-react/static/js/2.bd9ddd25.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/foodyrestaurants-react/static/js/2.bd9ddd25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58b9d9bc24e408b3b0e",
+    "url": "/foodyrestaurants-react/static/js/main.fbb6b49f.chunk.js"
+  },
+  {
+    "revision": "b08c35228a36d845073a",
+    "url": "/foodyrestaurants-react/static/js/runtime-main.951e2d57.js"
+  }
+]);
